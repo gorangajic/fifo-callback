5f9b83f633129d11bda3559d75de1e2de31e38de
Goran Gajic <fero.null@gmail.com>
2016-08-20 17:41:57 +0200

    mocha <3

--- test.js
+++ test.js
@@ -1,89 +1,94 @@
-import test from 'ava';
 import Fifo from './';
+import should from 'should';
 
-test('initial queue should be empty', t => {
-    const queue = new Fifo();
-    t.is(queue.length, 0);
+
+describe('fifo-callback', function() {
+
+    it('initial queue should be empty', () => {
+        const queue = new Fifo();
+        queue.should.have.length(0);
+    });
+
+    it('it should add callback to queue', () => {
+        const queue = new Fifo();
+        queue.push(() => {});
+
+        queue.should.have.length(1);
+    });
+
+    it('it should call callback immediately if queue empty', end => {
+        const queue = new Fifo();
+        queue.push(() => end());
+
+        queue.should.have.length(1);
+    });
+
+    it('it should remove from queue when done', end => {
+        const queue = new Fifo();
+        queue.push((done) => {
+            done();
+            queue.should.have.length(0);
+            end();
+        });
+
+        queue.should.have.length(1);
+    });
+
+    it('it should call one after another', end => {
+        const queue = new Fifo();
+        let i = 0;
+        queue.push((done) => {
+            i.should.be.equal(0);
+            i++;
+            setTimeout(done, 20);
+        });
+
+        queue.push((done) => {
+            i.should.be.equal(1);
+            done();
+            queue.should.have.length(0);
+            end();
+        });
+
+        queue.should.have.length(2);
+    });
+
+    it('it should call immediately after delay', end => {
+        const queue = new Fifo();
+        let i = 0;
+        queue.push(done => {
+            i.should.be.equal(0);
+            i++;
+            setTimeout(done, 20);
+        });
+
+        queue.push((done) => {
+            i.should.be.equal(1);
+            done();
+            queue.should.have.length(0);
+            setTimeout(() => {
+                queue.push(done => {
+                    done();
+                    end();
+                });
+            }, 20);
+        });
+
+        queue.should.have.length(2);
+    });
+
+
+    it('it should kill queue so next task is not run', end => {
+        const queue = new Fifo();
+        let i = 0;
+        queue.push(done => {
+            setTimeout(done, 30);
+            queue.kill();
+        });
+        queue.push(done => {
+            throw new Error('it should not be reached');
+        });
+        setTimeout(() => end(), 50);
+    });
+
 });
-
-test('it should add callback to queue', t=> {
-    const queue = new Fifo();
-    queue.push(() => {});
-
-    t.is(queue.length, 1);
-});
-
-test.cb('it should call callback immediately if queue empty', t=> {
-    const queue = new Fifo();
-    queue.push(() => t.end());
-
-    t.is(queue.length, 1);
-});
-
-test.cb('it should remove from queue when done', t => {
-    const queue = new Fifo();
-    queue.push((done) => {
-        done();
-        t.is(queue.length, 0);
-        t.end();
-    });
-
-    t.is(queue.length, 1);
-});
-
-test.cb('it should call one after another', t => {
-    const queue = new Fifo();
-    let i = 0;
-    queue.push((done) => {
-        t.is(i, 0);
-        i++;
-        setTimeout(done, 50);
-    });
-
-    queue.push((done) => {
-        t.is(i, 1);
-        done();
-        t.is(queue.length, 0);
-        t.end();
-    });
-
-    t.is(queue.length, 2);
-});
-
-test.cb('it should call immediately after delay', t => {
-    const queue = new Fifo();
-    let i = 0;
-    queue.push(done => {
-        t.is(i, 0);
-        i++;
-        setTimeout(done, 100);
-    });
-
-    queue.push((done) => {
-        t.is(i, 1);
-        done();
-        t.is(queue.length, 0);
-        setTimeout(() => {
-            queue.push(done => {
-                done();
-                t.end();
-            });
-        }, 100);
-    });
-
-    t.is(queue.length, 2);
-});
-
-
-test.cb('it should kill queue so next task is not run', t => {
-    const queue = new Fifo();
-    let i = 0;
-    queue.push(done => {
-        setTimeout(done, 30);
-        queue.kill();
-    });
-    queue.push(done => {
-        t.fail();
-    });
-    setTimeout(() => t.end(), 50);
-})
